12265492a5a6780d91dc9d65df21ead8aa136ddb
Charr003 <cargutierrez@estudiantec.cr>
2025-06-10 15:30:30 -0600

    Listado de Categoria

---
+++ Tienda_v1-main/src/main/java/com/Tienda/dao/ProductoDao.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+package com.tienda.dao;
+
+/**
+ *
+ * @author XPC
+ */
+
+import com.tienda.domain.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductoDao extends JpaRepository <Producto,Long> {
+    
+}
